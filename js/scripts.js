7cf3cff6bacfb98acab0a98c4364c9fc05078669
Ziedsabri <ziedsabri1920@gmail.com>
2024-12-10 18:19:54 +0100

    add

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+function togglePassword() {
+    const passwordField = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+    if (passwordField.type === 'password') {
+        passwordField.type = 'text';  // Show the password
+        eyeIcon.classList.remove('fa-eye');  // Remove eye icon
+        eyeIcon.classList.add('fa-eye-slash');  // Add eye-slash icon (for hide)
+    } else {
+        passwordField.type = 'password';  // Hide the password
+        eyeIcon.classList.remove('fa-eye-slash');  // Remove eye-slash icon
+        eyeIcon.classList.add('fa-eye');  // Add eye icon (for show)
+    }
+}
+
+
