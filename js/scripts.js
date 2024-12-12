ab2b66c19597742794e88d8a1f0f8db5a109aca0
azer20031 <ismailiazer1@gmail.com>
2024-12-12 20:13:36 +0100

    m

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function togglePassword() {
     const passwordField = document.getElementById('password');
     const eyeIcon = document.getElementById('eyeIcon');
     if (passwordField.type === 'password') {
-        passwordField.type = 'text';  // Show the password
-        eyeIcon.classList.remove('fa-eye');  // Remove eye icon
-        eyeIcon.classList.add('fa-eye-slash');  // Add eye-slash icon (for hide)
+        passwordField.type = 'text';  
+        eyeIcon.classList.remove('fa-eye');  
+        eyeIcon.classList.add('fa-eye-slash');  
     } else {
-        passwordField.type = 'password';  // Hide the password
-        eyeIcon.classList.remove('fa-eye-slash');  // Remove eye-slash icon
-        eyeIcon.classList.add('fa-eye');  // Add eye icon (for show)
+        passwordField.type = 'password';  
+        eyeIcon.classList.remove('fa-eye-slash');  
+        eyeIcon.classList.add('fa-eye');  
     }
 }
 
--- js/signup.js
+++ js/signup.js
@@ -3,13 +3,13 @@ function togglePassword(fieldId, iconId) {
     const passwordField = document.getElementById(fieldId);
     const eyeIcon = document.getElementById(iconId);
     if (passwordField.type === 'password') {
-        passwordField.type = 'text';  // Show the password
-        eyeIcon.classList.remove('fa-eye');  // Remove 'eye' icon
-        eyeIcon.classList.add('fa-eye-slash');  // Add 'eye-slash' icon
+        passwordField.type = 'text';  
+        eyeIcon.classList.remove('fa-eye');  
+        eyeIcon.classList.add('fa-eye-slash');  
     } else {
-        passwordField.type = 'password';  // Hide the password
-        eyeIcon.classList.remove('fa-eye-slash');  // Remove 'eye-slash' icon
-        eyeIcon.classList.add('fa-eye');  // Add 'eye' icon
+        passwordField.type = 'password';  
+        eyeIcon.classList.remove('fa-eye-slash');  
+        eyeIcon.classList.add('fa-eye');  
     }
 }
 
